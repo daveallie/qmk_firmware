ffb449690182bcc9f72847d7ea7bcc212bd848e3
Dave Allie <dave@daveallie.com>
2020-10-28 23:10:31 +1000

    Add daveallie lily58 keymap

---
+++ keyboards/lily58/keymaps/daveallie/rules.mk
@@ -0,0 +1,12 @@
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+EXTRAKEY_ENABLE = yes        # Audio control and System control
+CONSOLE_ENABLE = yes         # Console for debug
+OLED_DRIVER_ENABLE = yes     # OLED display
+ENCODER_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
